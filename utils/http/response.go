d58b304be50c2f9e22f1338d26dad446a4b79ca9
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2022-01-08 19:22:26 +0330

    worked on response.go

--- utils/http/response.go
+++ utils/http/response.go
@@ -1,37 +1,50 @@
 package http
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
-
-
+// this is the type of response that we esend from this program "always"
 type response struct {
-	Status int `json:"status"`
+	Status int         `json:"status"`
 	Result interface{} `json:"result"`
 }
 
-
-
-func newResponse() *response {
-
-}
-
-
-func (resp *response)  bytes() []byte{
+func newResponse(data interface{}, status int) *response {
+	return &response{
+		Status: status,
+		Result: data,
+	}
 
 }
 
-func (resp *response)  string() string{
-
+func (resp *response) bytes() []byte {
+	data, _ := json.Marshal(resp)
+	return data
 }
 
+func (resp *response) string() string {
+	return string(resp.bytes())
+}
 
-
-func (resp *response)  sendRequest() (w http.ResponseWriter , r *http.Request){
-
+func (resp *response) sendRequest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(resp.Status)
+	_, _ = w.Write(resp.bytes())
+	log.Println(resp.string())
 }
 
 
 //200
+func StatusOK() {
+
+}
+
+
+//204
 func StatusNoContent() {
 
 }
@@ -46,27 +59,22 @@ func StatusNotFound() {
 
 }
 
-
 //405
 func StatusMethodNotAllowed() {
 
 }
 
-
 //409
 func StatusConflict() {
 
 }
 
-
 //409
 func StatusConflict() {
 
 }
-
 
 //500
 func StatusIntervalServerError() {
 
 }
-
