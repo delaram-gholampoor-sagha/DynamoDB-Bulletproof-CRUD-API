3ce56d5b17dd49295524e7cbb9b490167f09b02d
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2022-01-09 16:22:03 +0330

    worked on health.go

--- internal/handlers/health/health.go
+++ internal/handlers/health/health.go
@@ -1,1 +1,45 @@
 package health
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/Delaram-Gholampoor-Sagha/DynamoDB-Bulletproof-CRUD-API/internal/handlers"
+	"github.com/Delaram-Gholampoor-Sagha/DynamoDB-Bulletproof-CRUD-API/internal/repository/adapter"
+)
+
+type Handler struct {
+	handlers.Interface
+	Repository adapter.Interface
+}
+
+func NewHnadler(repository adapter.Interface) handler.Interface {
+	return &Handler{
+		Repository: repository,
+	}
+}
+
+func (h *Handler) Get(w http.ResponseWriter, r *http.Request) {
+	if !h.Repository.Health() {
+		httpStatus.StatusInternalServerError(w, r, errors.New("Relational database not alive"))
+		return
+	}
+
+	HttpStatus.StatusOK(w, r, "Service Ok")
+}
+
+func Post() {
+
+}
+
+func Put() {
+
+}
+
+func Delete() {
+
+}
+
+func Options() {
+
+}
