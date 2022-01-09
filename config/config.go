2d1d83b18904e41e2117ee92e17ec2494e27dc93
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2022-01-09 14:12:55 +0330

    worked on our config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,32 @@
 package config
+
+import (
+	"strconv"
+
+	"github.com/Delaram-Gholampoor-Sagha/DynamoDB-Bulletproof-CRUD-API/utils/env"
+)
+
+type Config struct {
+	Port        int
+	Timeout     int
+	Dialect     string
+	DatabaseURI string
+}
+
+func GetConfig() Config {
+	return Config{
+		Port:        parseEnvToInt("PORT", "8080"),
+		Timeout:     parseEnvToInt("TIMEOUT", "30"),
+		Dialect:     env.GetEnv("DIALECT", "sqlite3"),
+		DatabaseURI: env.GetEnv("DATABASE_URI", ":memory:"),
+	}
+}
+
+func parseEnvToInt(envName string, defaultValue string) int {
+	num, err := strconv.Atoi(env.GetEnv(envName, defaultValue))
+	if err != nil {
+		return 0
+	}
+
+	return num
+}
